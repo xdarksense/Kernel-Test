7343ae0834d68b1c6e4488aaa88ec38b9da06ebc
anarkia1976 <stefano.villa1976@gmail.com>
2014-10-10 22:00:55 +0200

    ak-bacon-build: compile with new ToolChain Sabermod 4.9 - 20141010

--- ak-bacon-build.sh
+++ ak-bacon-build.sh
@@ -22,7 +22,7 @@ AK_VER="$BASE_AK_VER$VER"
 
 # Vars
 export LOCALVERSION=~`echo $AK_VER`
-export CROSS_COMPILE=${HOME}/android/AK-sm-arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=${HOME}/android/AK-sm-arm-eabi-4.9/bin/arm-eabi-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=ak
